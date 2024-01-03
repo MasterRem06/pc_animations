602c9a073d177dcd4b6c9f816f568826f988cf8b
Rémy <rem_tremblay@hotmail.com>
2024-01-03 17:28:23 -0500

    Electron project hello world

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 480,
+  })
+
+  mainWindow.loadFile('index.html')
+
+  mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
